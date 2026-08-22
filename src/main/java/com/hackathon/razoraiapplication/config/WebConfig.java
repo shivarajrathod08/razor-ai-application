e2860742d9b6182847d10e4ecb42d9c38e5a6f3a
Shivarajrathod08 <shivarajrathod12345@gmail.com>
2026-08-22 17:36:32 +0530

    Add WebConfig

--- src/main/java/com/hackathon/razoraiapplication/config/WebConfig.java
+++ src/main/java/com/hackathon/razoraiapplication/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.hackathon.razoraiapplication.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
